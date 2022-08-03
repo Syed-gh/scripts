e1448e4e3ddf6b52c4258f4ea6e54ad77ba2c993
Syed-gh <73746243+Syed-gh@users.noreply.github.com>
2022-08-03 17:58:27 +0500

    Update UI Library.lua

--- UI Library.lua
+++ UI Library.lua
@@ -1,1 +1,39 @@
+local Library = {}
+local Players = game:GetService("Players")
+local TweenService = game:GetService("TweenService")
 
+Library.create = function(classData)
+	local dataModel = Instance.new(classData.Data.Class)
+	dataModel.Parent = classData.Data.Parent
+	dataModel.Name = classData.Data.Name
+
+	if dataModel:IsA("ValueBase") then
+		dataModel.Value = classData.Data.Value
+	end
+
+	return classData.Data.Class .. " Created"
+end
+
+Library.GetUserId = function(Name)
+	local UserId = Players:GetUserIdFromNameAsync(Name)
+	return UserId
+end
+
+Library.GetName = function(UserId)
+	local Name = Players:GetNameFromUserIdAsync(UserId)
+	return Name
+end
+
+Library.Date = function(Pattern)
+	local Result = os.date(Pattern, os.time())
+	return Result
+end
+
+Library.Tween = function(GuiObj, Property, Value, Direction, Style, Duration)
+	-- implement TweenService
+	local nTweenInfo = TweenInfo.new(Duration, Enum.EasingStyle[Style], Enum.EasingDirection[Direction])
+	local Tween = TweenService:Create(GuiObj, nTweenInfo, {[Property] = Value})
+	Tween:Play()
+end
+
+return Library
