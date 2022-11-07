b48fa5ae9505f1f4abf67f80b095046ed8a03fa7
Syed-gh <73746243+Syed-gh@users.noreply.github.com>
2022-11-07 06:17:27 +0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -345,8 +345,8 @@ function UILibrary:Window(Table)
 				BackgroundTransparency = 1, 
 				BorderSizePixel = 1, 
 				Position = UDim2.new(0, 0,0, 0), 
-				Size = UDim2.new(1, 0,0.1, 0),
-				AutomaticSize = "Y",
+				Size = UDim2.new(1, 0,0.15, 0),
+				--AutomaticSize = "Y",
 				Visible = true, 
 
 			})
@@ -356,13 +356,15 @@ function UILibrary:Window(Table)
 			})
 			local sectionLabelFrame = lib.Create("Frame", sectionFrame, {
 				BackgroundColor3 = Color3.fromRGB(44, 44, 44), 
+				AnchorPoint = Vector2.new(0,0),
 				BackgroundTransparency = 1, 
 				BorderSizePixel = 0, 
 				Position = UDim2.new(0, 0,0, 0), 
-				Size = UDim2.new(1, 0,0.1, 0), -- scale y 0.1 
+				Size = UDim2.new(1, 0,0.85, 0), -- scale y 0.1 
 			})
 			local sectionlabel = lib.Create("TextLabel", sectionLabelFrame, {
 				BackgroundTransparency = 1, 
+				AnchorPoint = Vector2.new(0,0),
 				BorderSizePixel = 0, 
 				Position = UDim2.new(0, 0,0, 0), 
 				Size = UDim2.new(1, 0,1, 0),
